4d265a35f3f63e828da91bf4e35614b79db0d675
Gen2 <invisibro@cock.li>
2017-07-27 22:29:07 -0400

    typo

--- generic_lynxchan.sh
+++ generic_lynxchan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # EXTREMELY basic spamming script
-# Not intended for use with proxies. Run it by yourself or get multiple peoplt to run it.
+# Not intended for use with proxies. Run it by yourself or get multiple people to run it.
 # Thread ID should be specified like so: 22057
 # Timeout is specified by "sleep" 60 -> 60 seconds
 # Everything else explains itself.
